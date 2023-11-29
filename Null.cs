b214ba05f84ed835e2b25c0eaff90646d9832daf
olimov Amir <olimov.amir@mail.ru>
2023-11-29 20:27:23 +0300

    Добавьте файлы проекта.

---
+++ Null.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null
+{
+    class Null
+    {
+        class MyClass { }
+
+        struct MyStruct { }
+
+        static void Foo()
+        {
+            int[] a = new int[10];
+        }
+
+        static void Main(string[] args)
+        {
+            bool b;
+            int a;
+            double d;
+            Random random;
+            int[] arr;
+            MyClass myClass;
+            MyStruct myStruct;
+
+            Foo();
+
+        }
+    }
+}
